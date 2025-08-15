00068ef1146196b8aa4895b9620cd4053f59b901
pourquoi21 <miyaku219@naver.com>
2025-08-15 11:49:07 +0900

    #11 - JPA 테스트 작성
    기본 crud 연관관계 매핑과 cascading이 잘 작성하는지 보려고 작성
    테스트 데이터와 관련이 있기 때문에 이대로는 테스트가 실패할 것임

--- src/test/java/org/fc/fcboardwork/repository/JpaRepositoryTest.java
+++ src/test/java/org/fc/fcboardwork/repository/JpaRepositoryTest.java
@@ -42,6 +42,7 @@ class JpaRepositoryTest {
                 .isNotNull()
                 .hasSize(123);
     }
+
     @DisplayName("insert 테스트")
     @Test
     void givenTestData_whenInserting_thenWorksFine() {
@@ -54,5 +55,40 @@ class JpaRepositoryTest {
         // Then
         assertThat(articleRepository.count())
                 .isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#springboot";
+        article.setHashtag(updatedHashtag);
+
+        // When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        // Then
+        assertThat(savedArticle)
+                .hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count())
+                .isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count())
+                .isEqualTo(previousArticleCommentCount - deletedCommentsSize);
     }
 }
