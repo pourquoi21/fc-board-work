530bc077dac71a589abdff1e1ba1c16d31e6e011
jay <miyaku219@naver.com>
2025-08-18 11:26:54 +0900

    #17 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/test/java/org/fc/fcboardwork/controller/ArticleControllerTest.java
+++ src/test/java/org/fc/fcboardwork/controller/ArticleControllerTest.java
@@ -38,7 +38,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void given_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
